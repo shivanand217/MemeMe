10fbbb608ce9a0e42a94ceb0ef6c03f8b023cfa3
shiv anand <shivakp2111@gmail.com>
2019-02-15 23:21:32 +0530

    initial commit

---
+++ MemeME/Meme.swift
@@ -0,0 +1,17 @@
+//
+//  Meme.swift
+//  MemeME
+//
+//  Created by apple on 15/02/19.
+//  Copyright © 2019 shiv. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    var topText: String
+    var bottomText: String
+    var originalImage: UIImage
+    var memedImage: UIImage
+}
---
+++ MemeME/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemeME
+//
+//  Created by apple on 15/02/19.
+//  Copyright © 2019 shiv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
